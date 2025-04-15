3c8c4415d68700606556125aaecd9aae700f93c2
Lucien Shaw <myxlc55@outlook.com>
2025-04-15 21:23:28 +0800

    feat: 添加计划预约类，并修改了干预数据类的部分参数名称

--- labsniper/schedule.py
+++ labsniper/schedule.py
@@ -1,4 +1,10 @@
 import time
+from collections.abc import Iterable
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
+from .equipment import Equipment
+from .reservation import Hack, Reservation, ReservationService
+from .user import User
 
 TICKET_ALIVE_SECONDS = 30
 CONNECTION_ALIVE_SECONDS = 10
@@ -8,28 +14,28 @@ class Intervene:
     def __init__(
         self,
         target_timestamp: int | float,
-        creation_advance_seconds: int | float,
-        submission_advance_seconds: int | float,
+        creation_advance: int | float,
+        submission_advance: int | float,
         time_offset: int | float = 0,  # server_time - local_time
     ) -> None:
         self.param_check(target_timestamp, "目标时间戳")
-        self.param_check(creation_advance_seconds, "创建请求的提前秒数")
-        self.param_check(submission_advance_seconds, "提交请求的提前秒数")
+        self.param_check(creation_advance, "创建请求的提前秒数")
+        self.param_check(submission_advance, "提交请求的提前秒数")
         self.param_check(time_offset, "服务器时间校正偏移量", allow_negative=True)
 
-        if creation_advance_seconds - submission_advance_seconds > TICKET_ALIVE_SECONDS:
+        if creation_advance - submission_advance > TICKET_ALIVE_SECONDS:
             msg = "Invalid parameter. Detail:\n"
             msg += f"给定的创建请求和提交请求的时间间隔过长，建议设置为{TICKET_ALIVE_SECONDS}秒以内。"
             raise ValueError(msg)
 
-        if submission_advance_seconds > CONNECTION_ALIVE_SECONDS:
+        if submission_advance > CONNECTION_ALIVE_SECONDS:
             msg = "Invalid parameter. Detail:\n"
             msg += f"给定的提交请求的提前时间过长，建议设置为{CONNECTION_ALIVE_SECONDS}秒以内。"
             raise ValueError(msg)
 
         self.target_timestamp = target_timestamp
-        self.creation_advance_seconds = creation_advance_seconds
-        self.submission_advance_seconds = submission_advance_seconds
+        self.creation_advance = creation_advance
+        self.submission_advance = submission_advance
         self.time_offset = time_offset
 
     @staticmethod
@@ -58,12 +64,77 @@ class Intervene:
 
     def pause_before_request_creation(self) -> None:
         self.wait_until(
-            self.target_timestamp - self.creation_advance_seconds,
-            poll_interval=min(1, self.creation_advance_seconds / 10),
+            self.target_timestamp - self.creation_advance,
+            poll_interval=min(1, self.creation_advance / 10),
         )
 
     def pause_before_request_submission(self) -> None:
         self.wait_until(
-            self.target_timestamp - self.submission_advance_seconds,
-            poll_interval=min(0.001, self.submission_advance_seconds / 100),
+            self.target_timestamp - self.submission_advance,
+            poll_interval=min(0.001, self.submission_advance / 100),
         )
+
+
+class MultiReservationScheduler:
+    def __init__(
+        self,
+        target_timestamp: float | int,
+        creation_advance: float | int,
+        submission_advances: Iterable[float, int],
+        user: User,
+        equipment: Equipment,
+        reservation: Reservation,
+        time_offset: float | int = 0,
+        hack: Hack | None = None,
+    ) -> None:
+        self.service_args = {
+            "user": user,
+            "equipment": equipment,
+            "reservation": reservation,
+            "hack": hack,
+        }
+        self.intervene_args_all = [
+            {
+                "target_timestamp": target_timestamp,
+                "creation_advance": creation_advance,
+                "submission_advance": submission_advance,
+                "time_offset": time_offset,
+            }
+            for submission_advance in submission_advances
+        ]
+        self.num_jobs = len(self.intervene_args_all)
+        if self.num_jobs == 0:
+            msg = "Invalid parameter. Detail:\n"
+            msg += "没有可执行的任务，请检查输入参数。"
+            raise ValueError(msg)
+
+    def worker(self, service_args: dict, intervene_args: dict) -> str:
+        try:
+            intervene = Intervene(**intervene_args)
+            reservation_service = ReservationService(**service_args)
+            reservation_service.set_intervene(intervene)
+            return reservation_service.go()
+        except Exception as e:
+            return str(e)
+
+    def execute(self) -> None:
+        with ThreadPoolExecutor(max_workers=self.num_jobs) as executor:
+            future_map = {
+                executor.submit(
+                    self.worker,
+                    self.service_args,
+                    intervene_args,
+                ): {
+                    "idx": i,
+                    "submission_advance": intervene_args["submission_advance"],
+                }
+                for i, intervene_args in enumerate(self.intervene_args_all)
+            }
+            results = [None] * self.num_jobs
+            for future in as_completed(future_map):
+                idx = future_map[future]["idx"]
+                submission_advance = future_map[future]["submission_advance"]
+                results[idx] = future.result()
+                print(f"提交时间提前{submission_advance:.1f}秒的结果：\n{results[idx]}")
+                print("===" * 20)
+                print()
